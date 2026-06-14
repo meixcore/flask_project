b5cb61cba327d81486ddf60401728afd22bd1c76
meix <meixz@mail.ru>
2026-06-14 17:26:49 +0300

    Создание формы и обработка POST и GET запросов

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,23 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 from app import app
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("form.html")
+
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        color = request.form.get("color")
+        profession = request.form.get("profession")
+        hobbies = request.form.getlist("hobbies")
+        level = request.form.get("level")
+        return render_template("result.html", name=name, email=email, color=color, profession=profession, hobbies=hobbies, level=level)
+    else:
+        return redirect(url_for('form'))
 @app.route('/about')
 def about():
     return render_template("about.html")
