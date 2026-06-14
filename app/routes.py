0aef6ad58aa404bcce7aecca830bb96d96f00ee2
meix <meixz@mail.ru>
2026-06-14 16:46:19 +0300

    base.html

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from app import app
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 @app.route('/about')
 def about():
     return render_template("about.html")
